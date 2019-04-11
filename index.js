06a7f7b95badffaa136c94581c3707723c51f602
David Santos <kamalusantos@hotmail.com>
2019-04-10 20:43:45 -0600

    functional

--- index.js
+++ index.js
@@ -2,9 +2,23 @@
     $('form').submit(event => {
         event.preventDefault();
         let handle = $('#handle').val()
-        console.log(handle)
+        let url = "https://api.github.com/users/" + handle + "/repos"
+        callApi(url)
+
     })
  }
 
+ function callApi(query) {
+    fetch(query)
+        .then (response => response.json())
+        .then (responseJson => displayResults(responseJson))
+ }
 
+ function displayResults(jsonObject) {
+     console.log('displaying results')
+    $('.results').empty()
+    for (let i=0; i<jsonObject.length; i++) {
+        $('.results').append("<li><a href='" + jsonObject[i].svn_url + "' target='_blank'>" + jsonObject[i].name + "</a></li>")
+    }
+ }
 handleFormSubmit()
